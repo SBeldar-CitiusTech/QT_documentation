b8b030306e43e8b25cc7845c8c54d2108fc3ad0e
Sudhir Beldar <sudhir.beldar@citiustech.com>
2024-10-01 17:56:18 +0530

    partial response metrics

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const body = document.querySelector("body");
 const darkLight = document.querySelector("#darkLight");
 const sidebar = document.querySelector(".sidebar");
 const submenuItems = document.querySelectorAll(".submenu_item");
+const childmenuItems = document.querySelectorAll(".submenu");
 const sidebarOpen = document.querySelector("#sidebarOpen");
 // const sidebarClose = document.querySelector(".collapse_sidebar");
 // const sidebarExpand = document.querySelector(".expand_sidebar");
@@ -41,6 +42,17 @@ submenuItems.forEach((item, index) => {
     submenuItems.forEach((item2, index2) => {
       if (index !== index2) {
         item2.classList.remove("show_submenu");
+      }
+    });
+  });
+});
+
+childmenuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("show_childmenu");
+    childmenuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show_childmenu");
       }
     });
   });
